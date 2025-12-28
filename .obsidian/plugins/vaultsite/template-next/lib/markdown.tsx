0376704098ed1b1c97b72b7a0e0bd2f56a77f6e9
Ian <2005fong@gmail.com>
2025-12-27 16:54:47 -0800

    VaultSite publish: 2025-12-28T00:54:47.700Z

--- .obsidian/plugins/vaultsite/template-next/lib/markdown.tsx
+++ .obsidian/plugins/vaultsite/template-next/lib/markdown.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import remarkBreaks from 'remark-breaks';
@@ -8,7 +9,7 @@ export interface MarkdownRendererProps {
   content: string;
 }
 
-export function MarkdownRenderer({ content }: MarkdownRendererProps): JSX.Element {
+export function MarkdownRenderer({ content }: MarkdownRendererProps): React.JSX.Element {
   return (
     <ReactMarkdown
       remarkPlugins={[remarkGfm, remarkBreaks]}
